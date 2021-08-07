7e672d36fa0de8aef1a7e79ade58d64643e40103
TomF4 <75366232+TomF4@users.noreply.github.com>
2021-08-07 17:35:39 +0100

    created valid moves for pawn

--- Chess/src/com/company/Pieces/Pawn.java
+++ Chess/src/com/company/Pieces/Pawn.java
@@ -3,6 +3,7 @@ package com.company.Pieces;
 import com.company.Board;
 import com.company.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece{
@@ -17,9 +18,39 @@ public class Pawn extends Piece{
         super(isWhite, x, y);
     }
 
+    public Position[] pawnMoves =
+    {
+            new Position(0,2),  //Moves
+            new Position(0,-2), //Moves
+            new Position(0,1),  //Moves
+            new Position(0,-1), //Moves
+            new Position(-1,1), //Attack
+            new Position(1,1)   //Attack
+    };
+
+
+    /**
+     * Pawns can only move forward atm. (no special moves)
+     * @param board current board
+     * @return list of moves for pawns
+     */
     @Override
     public List<Position> findValidMoves(Board board) {
-        return null;
+        final List<Position> moves = new ArrayList<>();
+        Position potentialMove;
+
+        for(Position pos: pawnMoves){
+            potentialMove = Position.add(this.getPos(),pos);
+            if(potentialMove.getY() >= 0 && potentialMove.getX() >= 0) { //within board
+                if(this.isWhite() && pos.getY() > 0)
+                    moves.add(potentialMove);
+                else if(!this.isWhite() && pos.getY() < 0)
+                    moves.add(potentialMove);
+            }
+            System.out.println("All moves" + potentialMove);
+        }
+        System.out.println("Possible Moves" + moves.toString());
+        return moves;
     }
 
     @Override
