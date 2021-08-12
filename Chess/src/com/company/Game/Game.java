93b6bc99bef81726c3a91fab31acb46db8d28311
TomF4 <75366232+TomF4@users.noreply.github.com>
2021-08-12 13:45:13 +0100

    Make move and check legal move update

--- Chess/src/com/company/Game/Game.java
+++ Chess/src/com/company/Game/Game.java
@@ -1,27 +1,116 @@
 package com.company.Game;
 
 import com.company.Board.Board;
+import com.company.Pieces.Piece;
+import com.company.Pieces.PieceType;
+import com.company.Position;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * Game will run from this class
+ * Game class contains board start up and general rules
+ * This class does not yet contain ways of:
+ *      -Checking for check/checkmate
+ *      -Checking for obstruction on piece moves
+ *      -Castling and en passant
  * 08/08/2021
  */
 
-//enum of game state
 
 
 public class Game
 {
     private static GameStatus gameStatus = GameStatus.PLAYING;
+    private Board board;
+    private Player[] players;
+    private ArrayList<Move> movesPlayed;
+    private Player currentTurn;
 
     public static void main(String[] args)
     {
+    }
 
-        Board board = new Board();
+    /**
+     * initialises the board and player turn (resets the game)
+     */
+    private void initialise(Player player1,Player player2)
+    {
+        board.initBoard();
+        players[0] = player1;
+        players[1] = player2;
 
+        if(player1.isWhite())
+            this.currentTurn = player1;
+        else
+            this.currentTurn = player2;
+        movesPlayed.clear();
+        setGameStatus(GameStatus.PLAYING);
+    }
+
+    //TODO maybe change this so it takes tiles instead of position do u need the player argument when there is a currentTurn???
+    /**
+     * this method will move the piece
+     * @param currentPlayer current player
+     * @param start starting tile pos
+     * @param end ending tile pos
+     * @return true when move complete
+     */
+    private boolean makeMove(Player currentPlayer, Position start, Position end)
+    {
+        //create the move
+        Move move = new Move(currentPlayer, board.getTile(start),board.getTile(end));
+
+        //get the piece to be moved from board array
+        Piece movingPiece = board.getTile(start).getPiece();
+
+        //check to see if player is moving same colour piece
+        if(!checkIfLegalMove(movingPiece,start,end))
+            return false;
+
+        return true;
+    }
+
+    //TODO check whether piece is obstructed!!!!!!!!!!!!!
+    /**
+     * checks to see if the moves is within board, not same colour and whether piece is obstructed.
+     * Method is most likely only used in "makeMove"
+     * @param piece piece being moved
+     * @param start tile pos start
+     * @param end ending pos
+     * @return true if move is possible
+     */
+    private boolean checkIfLegalMove(Piece piece,Position start,Position end)
+    {
+        //get valid moves
+        List<Position> validMoves = piece.findValidMoves(board);
+        for (Position movePos: validMoves)
+        {
+            //bunch of move validity checking
+            if(movePos == end) // moves is in the list
+                if(end.getY() >= 0 && end.getX() >= 0 && end.getX() < 8 && end.getY() < 8) //moves is within board
+                    if (piece.isWhite() != board.getTile(end).getPiece().isWhite()) { // end piece is not same colour
+                        if(piece.getPieceType() == PieceType.KNIGHT) // knight cant be obstructed
+                            return true;
+                    }
+        }
+        return false;
+    }
+
+    public static GameStatus getGameStatus()           { return gameStatus;   }
+    public static void setGameStatus(GameStatus status){ gameStatus = status; }
+
+}
+
+
+
+
+
+
+/*
+     Board board = new Board();
+        Move move;
         Player whitePlayer = new Player(true);
         Player blackPlayer = new Player(false);
 
@@ -55,9 +144,4 @@ public class Game
 
             System.out.println();
         }
-    }
-
-    public static GameStatus getGameStatus()           { return gameStatus;   }
-    public static void setGameStatus(GameStatus status){ gameStatus = status; }
-
-}
+ */
